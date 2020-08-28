76437b8ee143cebf52eccba5e832e9e1d2d1009d
Amir2170 <amirrezaamp2170@gmail.com>
2020-08-29 01:00:25 +0430

    front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -16,24 +16,24 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.get('http://localhost:8000')
 		#page title and header mention 'To-Do'
 		self.assertIn('To-Do', self.browser.title)
-		header_text = self.browser.find_element_by_tag_name('h1')
-		self.assertIn(header_text, 'To-Do')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 		#user is invited to enter a to-do list straight away
-		inputbox = self.browser.find_element_by_id('id_list_item')
+		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(inputbox.get_attribute('placeholder'), 
 			'Enter a to-do item')
 		#user types smt into text box
 		inputbox.send_keys('1: Buy peacock feathers')
 		#when user press Enter page will refresh and 
 		#put to-do list in specified text into the list
-		inputbox.send_keys(Keys.Enter)
+		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
 		
-		table = find_element_by_id('id_list_table')
-		rows = find_elements_by_tag_name('tr')
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = self.browser.find_elements_by_tag_name('tr')
 		self.assertTrue(
 			any(row.text == '1: Buy peacock feathers' for row in rows)
-			)
+			, "New to-do item did not appear in the list")
 		
 		self.fail('Finish Functional Test!')
 		

8f41031d8f0e8a93ce9bcb8f6b2c704d928f8b4c
Amir2170 <amirrezaamp2170@gmail.com>
2020-08-30 23:01:08 +0430

    redirect after post and show all items in templates

--- lists/tests.py
+++ lists/tests.py
@@ -11,10 +11,30 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html') 
         
-    def test_can_dave_POST_request(self):
+    def test_can_save_POST_request(self):
+        self.client.post('/', data={'item_text': 'A new list item'})
+        
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+    
+    def test_redirects_after_post(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+    
+    def test_only_saves_items_when_necessary(self):
+        response = self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+        
+    def test_display_all_list_items(self):
+        Item.objects.create(text='Itemy 1')
+        Item.objects.create(text='Itemy 2')
+        
+        response = self.client.get('/')
+        
+        self.assertIn('Itemy 1', response.content.decode())
+        self.assertIn('Itemy 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
